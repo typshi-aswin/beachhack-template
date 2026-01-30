9c6fa7d4dae141289e8f3a398a2b43756e0a6160
Aswin V Sivan <aswinvsivan@gmail.com>
2026-01-30 20:50:19 +0530

    feat: second big commit

---
+++ frontend/src/application/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import styles from './Dashboard.module.css';
+import MainCardLayout from '../../components/MainCardLayout/MainCardLayout';
+
+const Dashboard = () => {
+    return (
+        <MainCardLayout>
+            <div className={styles.container}>
+                Hi
+            </div>
+        </MainCardLayout>
+    );
+}
+
+export default Dashboard;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+import { router } from './router.tsx';
+import { RouterProvider } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { toasterProps } from './common/constants.ts';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <Toaster {...toasterProps} />
   </StrictMode>,
 )
